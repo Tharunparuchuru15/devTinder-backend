59f220b6aca64611e1846471b29acd92e4961fbe
Tharun <tharunparuchuru1512@gmail.com>
2025-03-14 10:39:13 +0530

    next and route handlers

--- learn.md
+++ learn.md
@@ -5,3 +5,7 @@ Should we pass package-lock.json to repository
 why is browser a bad way to test apis?
 how to test apis in browser in the first place?
 
+try regex in the routes
+read more about quer params in the routes 
+read about dynamic routes
+
--- src/app.js
+++ src/app.js
@@ -3,13 +3,35 @@ const express= require('express')
 const app = express()
 
 app.use("/test",(req,res)=>{
+
     res.send("Hello from the test serverr")
 })
 
-app.get("/user", (req, res)=>{
-    res.send("user sent")
+
+
+app.get("/user",
+    [
+ (req, res, next)=>{
+    console.log(req.params)
+     next()
     
-})
+},
+(req, res, next)=>{
+    console.log(req.params)
+    // res.send("user  2")
+    next()
+}], 
+(req, res)=>{
+    console.log(req.params)
+    res.send("user  3")
+    
+},
+(req, res)=>{
+    console.log(req.params)
+    res.send("user  4")
+    
+}
+)
 
 app.post("/user", (req,res)=>{
     res.send("user created")
